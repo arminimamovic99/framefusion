0db8369f87e74b680920029aa21901b65df13f4c
arminimamovic99 <aimamovic99@outlook.com>
2024-01-16 23:25:12 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'agency';
+  title = 'FrameFusion';
+
+  rotateIcon(id: string) {
+    const icon = document.getElementById(id);
+    if (icon?.classList.contains('rotate-back')) {
+      icon.classList.remove('rotate-back');
+    }
+
+    if (icon?.classList.contains('rotate-45')) {
+      icon.classList.remove('rotate-45');
+      icon.className += ' rotate-back';
+      return;
+    }
+
+    if (!icon) return;
+    icon.className += ' rotate-45';
+  }
 }
